faa2908e240e7970e9f587b2b46147a9b68d58c2
JSGehlen <jeremygehlen570@gmail.com>
2020-07-01 14:29:16 +0300

    Ran tests for App and fixed a typo in data-testid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
-
 import { ProjectsProvider, SelectedProjectProvider } from './context';
 
-export const  App = ({darkModeDefault = false }) => {
- const [ darkMode, setDarkMode ] = useState(darkModeDefault);
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
 
   return (
     <SelectedProjectProvider>
       <ProjectsProvider>
-        <main 
-          data-test="application"
+        <main
+          data-testid="application"
           className={darkMode ? 'darkmode' : undefined}
         >
-          <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
-          <Content/>
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
         </main>
       </ProjectsProvider>
     </SelectedProjectProvider>
-)};
+  );
+};
